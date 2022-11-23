04371cb4c23f37ed869529dcb7c3d0189ff0ae97
kev127 <kelvinkeya125@gmail.com>
2022-11-23 22:57:39 +0300

    updates

--- server.js
+++ server.js
@@ -41,6 +41,6 @@ app.use('/image', image);
 
  
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,() =>{
+module.exports = app.listen(PORT,() =>{
     console.log(`Server is listening at http://localhost:${PORT}`)
 });
