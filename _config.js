3556e6e45009b1e6b671254a876aed96e2e3e4b7
kev127 <kelvinkeya125@gmail.com>
2022-11-24 09:27:40 +0300

    updates

--- _config.js
+++ _config.js
@@ -2,9 +2,9 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<kev125>:<4789>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<kev125>:<4789>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<kev125>:<4789>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://kev125:<YJGOTqxh2KEKZJn9>@gallery.sipdakz.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://kev125:<YJGOTqxh2KEKZJn9>@gallery.sipdakz.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://kev125:<YJGOTqxh2KEKZJn9>@gallery.sipdakz.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
  
