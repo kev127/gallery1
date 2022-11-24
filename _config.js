3f6c625ecbe00b8ad98bb5e36785e51b14d0d5a3
kev127 <kelvinkeya125@gmail.com>
2022-11-24 10:54:06 +0300

    update config file

--- _config.js
+++ _config.js
@@ -2,9 +2,9 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://kev125:<YJGOTqxh2KEKZJn9>@gallery.sipdakz.mongodb.net/?retryWrites=true&w=majority',
-    development: 'mongodb+srv://kev125:<YJGOTqxh2KEKZJn9>@gallery.sipdakz.mongodb.net/?retryWrites=true&w=majority',
-    test: 'mongodb+srv://kev125:<YJGOTqxh2KEKZJn9>@gallery.sipdakz.mongodb.net/?retryWrites=true&w=majority',
+    production: 'mongodb+srv://kev125:YJGOTqxh2KEKZJn9@gallery.sipdakz.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://kev125:YJGOTqxh2KEKZJn9@gallery.sipdakz.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://kev125:YJGOTqxh2KEKZJn9@gallery.sipdakz.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
  
